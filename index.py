f3bfd5eb90b326b78f8455c12244ff24687e6ac1
Brian <banebeetle@gmail.com>
2025-02-21 22:07:02 -0800

    First commit

---
+++ index.py
@@ -0,0 +1,14 @@
+# Brian Phan
+# 84609992
+# A3 Indexer
+
+'''Create an inverted index for the corpus with data structures designed by you.
+• Tokens: all alphanumeric sequences in the dataset.
+• Stop words: do not use stopping while indexing, i.e. use all words, even
+the frequently occurring ones.
+• Stemming: use stemming for better textual matches. Suggestion: Porter
+stemming, but it is up to you to choose.
+• Important text: text in bold (b, strong), in headings (h1, h2, h3), and
+in titles should be treated as more important than the in other places.
+Verify which are the relevant HTML tags to select the important words.'''
+

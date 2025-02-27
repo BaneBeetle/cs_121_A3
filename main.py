0c0347488ee4c244d0e1d6075106a42561f2f5bc
Galilea Ruiz <galileanruiz@gmail.com>
2025-02-26 23:48:06 -0800

    forgot to add new files to repo

---
+++ main.py
@@ -0,0 +1,5 @@
+# entry point to indexing and retrieving
+
+from flask import Flask, request, jsonify
+
+
---
+++ search.py
@@ -0,0 +1,33 @@
+# Implements Boolean retrieval (Milestone 2)
+
+import json
+import sys
+import re
+
+
+def load_index():
+    ''' Load the inverted index from the disk '''
+    pass
+
+
+def process_query():
+    """
+        Process a Boolean query using the inverted index.
+        Should support AND, OR, and NOT operations.
+        """
+    pass
+
+
+def evaluate_expressions():
+    '''
+    Evaluate Boolean expressions
+    '''
+    pass
+
+
+if __name__ == "__main__":
+    index = load_index()
+    # while True:
+    # enter query using Boolean values and exit if 'exit' is input
+    # process_query
+    # print / return documents matching queries
